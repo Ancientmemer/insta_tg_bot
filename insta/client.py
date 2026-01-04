f1c125992518c9e7b01112794ba6d4208eb3f519
Ancientmemer <ancientmemer12@gmail.com>
2026-01-04 17:35:13 +0530

    Update client.py

--- insta/client.py
+++ insta/client.py
@@ -1,23 +1,20 @@
 from instagrapi import Client
-import os, json
+import os
 
 SESSION_FILE = "data/session.json"
 
 cl = Client()
 
-def is_logged_in():
-    return os.path.exists(SESSION_FILE)
-
-def login(username, password):
-    cl.login(username, password)
-    cl.dump_settings(SESSION_FILE)
-
 def load_session():
-    if os.path.exists(SESSION_FILE):
+    if os.path.exists(SESSION_FILE) and os.path.getsize(SESSION_FILE) > 0:
         cl.load_settings(SESSION_FILE)
         cl.login_by_sessionid(cl.sessionid)
         return True
     return False
+
+def login(username, password):
+    cl.login(username, password)
+    cl.dump_settings(SESSION_FILE)
 
 def logout():
     if os.path.exists(SESSION_FILE):
