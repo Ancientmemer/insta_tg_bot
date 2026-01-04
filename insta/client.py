e2abe4ca0234727184b0af5b255213b92ff8b651
Ancientmemer <ancientmemer12@gmail.com>
2026-01-04 21:37:11 +0530

    Update client.py

--- insta/client.py
+++ insta/client.py
@@ -5,6 +5,12 @@ SESSION_FILE = "data/session.json"
 
 cl = InstaClient()
 
+
+def _reset_client():
+    global cl
+    cl = InstaClient()
+
+
 def load_session():
     if os.path.exists(SESSION_FILE) and os.path.getsize(SESSION_FILE) > 0:
         cl.load_settings(SESSION_FILE)
@@ -12,13 +18,33 @@ def load_session():
         return True
     return False
 
+
 def login(username, password):
+    global cl
+
+    # reset client before login (VERY IMPORTANT)
+    _reset_client()
+
     cl.login(username, password)
     cl.dump_settings(SESSION_FILE)
 
+
 def logout():
+    global cl
+
+    try:
+        # logout from instagram side
+        cl.logout()
+    except:
+        pass
+
+    # remove local session
     if os.path.exists(SESSION_FILE):
         os.remove(SESSION_FILE)
+
+    # reset client completely
+    _reset_client()
+
 
 def is_logged_in():
     return os.path.exists(SESSION_FILE) and os.path.getsize(SESSION_FILE) > 0
